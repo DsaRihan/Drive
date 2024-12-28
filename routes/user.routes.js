e44959078fc073faea5a2cd80b7fdac8a565b3dc
Rihan <100439488+DsaRihan@users.noreply.github.com>
2024-12-28 23:18:54 +0530

    hashing of password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { body,validationResult } = require('express-validator');
+const userModel = require('../models/usermodel')
+const bcrypt = require('bcrypt');
 
 router.get('/register',(req,res)=>{
     res.render('register')
@@ -10,7 +12,7 @@ router.post('/register',
     body('email').trim().isEmail(),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3}),
-    (req,res)=>{
+    async (req,res)=>{
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({
@@ -18,7 +20,49 @@ router.post('/register',
                 message:"Invalid Data"
             })
         }
-        res.send(errors)
+        const{email,password,username} = req.body
+        const hashedPassword = await bcrypt.hash(password,10)   //hashing the password
+        const newuser = await userModel.create({
+            email,
+            password : hashedPassword,
+            username
+        })
+        res.json(newuser)
 })
+
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+router.post('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}),
+    async (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:"Invalid Data",
+                status:400
+            })
+        }
+        const {username,password} = req.body
+        const hashpass = await bcrypt.hash(password,10)
+        const user = await userModel.findOne({
+            username:username
+        })
+        if(!user){
+            return res.status(400).json({
+                message:"username or password is incorrect"
+            })
+        }
+        const ismatch = await bcrypt.compare(password,user.password)
+        if(!ismatch){
+            return res.status(400).json({
+                message:"username or password is incorrect"
+            })
+        }
+    }
+)
 
 module.exports = router;
