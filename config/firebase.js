4d42f26f6356579d4759d62387420b46a78de793
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-07 10:25:39 +0530

    firebase added

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+const Firebase = require("firebase-admin");
+const serviceacc = require("../drive-c91ad-firebase-adminsdk-lez4u-e3b20ea7b5.json")
+
+const firebase = Firebase.initializeApp({
+    credential : Firebase.credential.cert(serviceacc),
+    storageBucket : "drive-c91ad.appspot.com"
+})
+
+module.exports = firebase;
